89684503e15812fac40cb9bd63e06d7fd8771742
zeytzer <zeytzer@gmail.com>
2024-10-10 22:29:35 +0300

    Remove unused page components

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -20,8 +20,8 @@ export default async function Page({ params }: Params) {
   ]);
 
   return (
-    <div className="flex items-center justify-center flex-col w-screen min-h-screen pb-10 gap-8 sm:pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] uppercase select-none">
-      <div className="flex items-center justify-center flex-col bg-black gap-10 w-full sm:w-screen max-h-lg sm:max-h-[600px] p-8">
+    <div className="flex items-center justify-center flex-col min-h-screen w-screen gap-8 font-[family-name:var(--font-geist-sans)] uppercase select-none">
+      <div className="flex items-center justify-center flex-col bg-black gap-10 w-full w-full  sm:max-h-[600px] p-8">
         <h1 className="text-2xl sm:text-4xl font-bold text-center">
           Name: {age?.name}
         </h1>
@@ -32,10 +32,10 @@ export default async function Page({ params }: Params) {
           Gender: {gender?.gender}
         </h1>
       </div>
-      <Link href="/">
+      <Link href="/" className="fixed bottom-10">
         <button
           type="submit"
-          className="text-center text-lg sm:text-2xl border-2 border-white p-4 h-16 w-48 sm:h-20 sm:w-60 rounded-2xl hover:bg-white hover:text-black"
+          className="text-center text-xl sm:text-2xl border-2 border-white p-4 h-16 w-48 sm:h-20 sm:w-60 rounded-2xl hover:bg-white hover:text-black  "
         >
           GO BACK
         </button>
